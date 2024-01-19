Fixed normalization on input to [0,1] and swapped to MSE loss

dflat/metasurface/datasets.py
@@ -19,13 +19,13 @@ class Nanofins_TiO2_U350nm_H600nm(Dataset):
 
         # Raw phase and transmittance data has shape [Npol=2, leny=49, lenx=49, wavelength=441]
         data = scipy.io.loadmat(datpath)
-        self.phase = np.angle(np.exp(1j * data["phase"])) + np.pi
-        self.trans = np.sqrt(data["transmission"])
+        self.phase = data["phase"]
+        self.trans = np.sqrt(np.clip(data["transmission"], 0, np.finfo(np.float32).max))
         self.params = [data["leny"], data["lenx"], data["wavelength_m"].flatten()]
         self.param_limits = [[60e-9, 300e-9], [60e-9, 300e-9], [310e-9, 750e-9]]
 
         # Transform the data into a cell-level dataset ([0, 1])
-        trans = np.clip(self.trans, 0, np.finfo(np.float32).max)
+        trans = self.trans
         phase = self.phase
         params = np.meshgrid(
             *[
@@ -38,14 +38,16 @@ class Nanofins_TiO2_U350nm_H600nm(Dataset):
         self.y = np.stack(
             [
                 trans[0].flatten(),
-                np.cos(phase[0].flatten()),
-                np.sin(phase[0].flatten()),
+                (np.cos(phase[0].flatten()) + 1) / 2,
+                (np.sin(phase[0].flatten()) + 1) / 2,
                 trans[1].flatten(),
-                np.cos(phase[1].flatten()),
-                np.sin(phase[1].flatten()),
+                (np.cos(phase[1].flatten()) + 1) / 2,
+                (np.sin(phase[1].flatten()) + 1) / 2,
             ],
             -1,
         )
+        print(self.x.shape, self.y.shape)
+        print(self.x.min(), self.x.max(), self.y.min(), self.y.max())
 
     def __len__(self):
         return self.x.shape[0]
@@ -132,13 +134,13 @@ class Nanocylinders_TiO2_U180nm_H600nm(Dataset):
 
         # Phase and transmission has shape [wavelength=441, lenr=191]
         data = scipy.io.loadmat(datpath)
-        self.phase = np.angle(np.exp(1j * data["phase"])) + np.pi
-        self.trans = np.sqrt(data["transmission"])
+        self.phase = data["phase"]
+        self.trans = np.sqrt(np.clip(data["transmission"], 0, np.finfo(np.float32).max))
         self.params = [data["radius_m"], data["wavelength_m"].flatten()]
         self.param_limits = [[30e-9, 150e-9], [310e-9, 750e-9]]
 
         # Transform data into a cell-level dataset ([0, 1])
-        trans = np.clip(self.trans, 0, np.finfo(np.float32).max)
+        trans = self.trans
         phase = self.phase
         params = np.meshgrid(
             *[
@@ -151,11 +153,13 @@ class Nanocylinders_TiO2_U180nm_H600nm(Dataset):
         self.y = np.stack(
             [
                 trans.flatten(),
-                np.cos(phase.flatten()),
-                np.sin(phase.flatten()),
+                (np.cos(phase.flatten()) + 1) / 2,
+                (np.sin(phase.flatten()) + 1) / 2,
             ],
             -1,
         )
+        print(self.x.shape, self.y.shape)
+        print(self.x.min(), self.x.max(), self.y.min(), self.y.max())
 
     def __len__(self):
         return self.x.shape[0]
@@ -165,12 +169,12 @@ class Nanocylinders_TiO2_U180nm_H600nm(Dataset):
 
     def plot(self, savepath=None):
         phase = self.phase
-        trans = self.transmittance
+        trans = self.trans
         r = self.params[0] * 1e9
         lam = self.params[1] * 1e9
 
         fig, ax = plt.subplots(1, 2, figsize=(12, 5))
-        ax[0].imshow(trans, vmin=0, vmax=1)
+        ax[0].imshow(trans)
         formatPlot(
             fig,
             ax[0],
@@ -182,7 +186,7 @@ class Nanocylinders_TiO2_U180nm_H600nm(Dataset):
             addcolorbar=True,
             setAspect="auto",
         )
-        ax[1].imshow(phase, vmin=0, vmax=2 * np.pi, cmap="hsv")
+        ax[1].imshow(phase, cmap="hsv")
         formatPlot(
             fig,
             ax[1],
@@ -207,13 +211,13 @@ class Nanoellipse_TiO2_U350nm_H600nm(Nanofins_TiO2_U350nm_H600nm):
         data = scipy.io.loadmat(datapath)
 
         # Phase and transmission has shape [Npol=2, leny=49, lenx=49, wavelength=441]
-        self.phase = np.angle(np.exp(1j * data["phase"])) + np.pi
-        self.transmittance = np.sqrt(data["transmission"])
+        self.phase = data["phase"]
+        self.trans = np.sqrt(np.clip(data["transmission"], 0, np.finfo(np.float32).max))
         self.params = [data["lenx"], data["leny"], data["wavelength_m"].flatten()]
         self.param_limits = [[60e-9, 300e-9], [60e-9, 300e-9], [310e-9, 750e-9]]
 
         # Transform the data into a cell-level dataset ([0, 1])
-        trans = np.clip(self.trans, 0, np.finfo(np.float32).max)
+        trans = self.trans
         phase = self.phase
         params = np.meshgrid(
             *[
@@ -226,11 +230,13 @@ class Nanoellipse_TiO2_U350nm_H600nm(Nanofins_TiO2_U350nm_H600nm):
         self.y = np.stack(
             [
                 trans[0].flatten(),
-                np.cos(phase[0].flatten()),
-                np.sin(phase[0].flatten()),
+                (np.cos(phase[0].flatten()) + 1) / 2,
+                (np.sin(phase[0].flatten()) + 1) / 2,
                 trans[1].flatten(),
-                np.cos(phase[1].flatten()),
-                np.sin(phase[1].flatten()),
+                (np.cos(phase[1].flatten()) + 1) / 2,
+                (np.sin(phase[1].flatten()) + 1) / 2,
             ],
             -1,
         )
+        print(self.x.shape, self.y.shape)
+        print(self.x.min(), self.x.max(), self.y.min(), self.y.max())

dflat/metasurface/nn.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 
 class SirenNet(nn.Module):
     def __init__(
-        self, in_channels, out_channels, channels=(256, 256), w0=100.0, w0_initial=30
+        self, in_channels, out_channels, channels=(256, 256), w0=1.0, w0_initial=30
     ):
         super().__init__()
         self.blocks = nn.ModuleList()

dflat/metasurface/optical_model.py
@@ -4,18 +4,12 @@ from einops import rearrange
 from .load_utils import instantiate_from_config
 
 
-def disabled_train(self, mode=True):
-    """Overwrite model.train with this function to make sure train/eval mode
-    does not change anymore."""
-    return self
-
-
 class NeuralCells(nn.Module):
     def __init__(self, nn_config, param_bounds, trainable_model=False):
         super().__init__()
         self.model = self._initialize_model(nn_config, trainable_model)
         self.param_bounds = param_bounds
-        self.loss = nn.L1Loss()
+        self.loss = nn.MSELoss()  # This outperforms L1 loss
 
     def training_step(self, x, y):
         pred = self.model(x)
@@ -65,7 +59,9 @@ class NeuralCells(nn.Module):
         )
 
         out = torch.complex(out[..., 0], torch_zero) * torch.exp(
-            torch.complex(torch_zero, torch.atan2(out[..., 2], out[..., 1]))
+            torch.complex(
+                torch_zero, torch.atan2((out[..., 2] * 2) - 1, (out[..., 1] * 2) - 1)
+            )
         )
 
         return torch.abs(out), torch.angle(out)
@@ -77,7 +73,6 @@ class NeuralCells(nn.Module):
 
         if not trainable_model:
             model = model.eval()
-            model.train = disabled_train
             for param in model.parameters():
                 param.requires_grad = False
 