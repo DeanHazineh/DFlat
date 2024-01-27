88649b7ca311f952bcfeff203bedd84a81830725
Dean Hazineh <dhazineh@g.harvard.edu>
2024-01-27 12:41:08 -0500

    Update load module for absolute pathing and relative pathing

--- dflat/metasurface/load_utils.py
+++ dflat/metasurface/load_utils.py
@@ -13,8 +13,7 @@ def load_optical_model(config_path):
     Returns:
         nn.Module: Model with pretrained weights loaded if ckpt_path is specified in the config file.
     """
-    config_path = pkg_resources.resource_filename("dflat", config_path)
-    config = OmegaConf.load(config_path)
+    config = load_config_from_path(config_path)
     ckpt_path = config.model["ckpt_path"]
     optical_model = instantiate_from_config(config.model, ckpt_path, strict=True)
     return optical_model
@@ -29,14 +28,17 @@ def load_trainer(config_path):
     Returns:
         object: Trainer
     """
-    config_path = pkg_resources.resource_filename("dflat", config_path)
-    print(f"Loading trainer from config path: {config_path}")
-
-    config = OmegaConf.load(config_path)
+    config = load_config_from_path(config_path)
     config_model = config.model
-
     config_trainer = config.trainer
-    ckpt_path = pkg_resources.resource_filename("dflat", config_trainer["ckpt_path"])
+
+    rel_ckpt = config_trainer["relative_ckpt"]
+    ckpt_path = config_trainer["ckpt_path"]
+    ckpt_path = (
+        ckpt_path
+        if not rel_ckpt
+        else pkg_resources.resource_filename("dflat", ckpt_path)
+    )
     dataset = get_obj_from_str(config_trainer["data"])()
 
     trainer = get_obj_from_str(config_trainer["target"])(
@@ -48,20 +50,41 @@ def load_trainer(config_path):
     return trainer
 
 
+def load_config_from_path(config_path):
+    try:
+        use_path = pkg_resources.resource_filename("dflat", config_path)
+        config = OmegaConf.load(use_path)
+    except Exception as e1:
+        try:
+            use_path = config_path
+            config = OmegaConf.load(use_path)
+        except Exception as e2:
+            print(f"Failed absolute path identification. Errors \n {e1} \n {e2}.")
+
+    return config
+
+
 def instantiate_from_config(config_model, ckpt_path=None, strict=False):
     assert "target" in config_model, "Expected key `target` to instantiate."
     target_str = config_model["target"]
     print(f"Target Module: {target_str}")
     loaded_module = get_obj_from_str(target_str)(**config_model.get("params", dict()))
 
-    # Get model checkpoint
     if ckpt_path is not None and ckpt_path != "None":
-        ckpt_path = pkg_resources.resource_filename("dflat", ckpt_path)
-        print(
-            f"Target: {config_model['target']} Loading from checkpoint {ckpt_path} as strict={strict}"
-        )
-        sd = torch.load(ckpt_path, map_location="cpu")["state_dict"]
+        ## Try and Except to handle relative pathing vs absolute pathing
+        try:
+            use_path = pkg_resources.resource_filename("dflat", ckpt_path)
+            sd = torch.load(use_path, map_location="cpu")["state_dict"]
+        except Exception as e1:
+            try:
+                use_path = ckpt_path
+                sd = torch.load(use_path, map_location="cpu")["state_dict"]
+            except Exception as e2:
+                print(f"Failed absolute path identification. Errors \n {e1} \n {e2}.")
 
+        print(
+            f"Target: {config_model['target']} Loading from checkpoint {use_path} as strict={strict}"
+        )
         missing, unexpected = loaded_module.load_state_dict(sd, strict=strict)
         print(
             f"Restored {target_str} with {len(missing)} missing and {len(unexpected)} unexpected keys"
