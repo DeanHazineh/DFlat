634244a5ce9765238a02270ff2dd1df559b2aefc
Dean Hazineh <dhazineh@g.harvard.edu>
2024-01-29 19:58:46 -0500

    added rest of renderer minus noise model EMVA

--- dflat/plot_utilities/__init__.py
+++ dflat/plot_utilities/__init__.py
@@ -1,1 +1,2 @@
 from .mp_format import formatPlot
+from .vis import vis_hyperspectral_image
---
+++ dflat/render/__init__.py
@@ -0,0 +1,3 @@
+from .fft_convolve import general_convolve
+from .fronto_planar_renderer import Fronto_Planar_Renderer_Incoherent
+from .util_meas import hsi_to_rgb, photons_to_ADU
