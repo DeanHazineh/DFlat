4b7a35f947f6d62f98ed87ea534f47dd55feff81
Dean Hazineh <dhazineh@g.harvard.edu>
2024-01-29 20:41:05 -0500

    Remove and replace bad dataset with newly generated data

--- dflat/metasurface/datasets.py
+++ dflat/metasurface/datasets.py
@@ -1,4 +1,5 @@
 import scipy.io
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,6 +12,174 @@ from dflat.plot_utilities.mp_format import formatPlot
 def get_path_to_data(file_name: str):
     resource_path = Path(__file__).parent / "data/"
     return resource_path.joinpath(file_name)
+
+
+class Nanocylinders_TiO2_U300nm_H600nm(Dataset):
+    def __init__(self):
+        datpath = get_path_to_data(
+            "Nanocylinders_TiO2_Unit300nm_height600nm_FDTD.pickle"
+        )
+
+        with open(datpath, "rb") as file:
+            # Load the data from the file
+            data = pickle.load(file)
+            rvec = data["rvec"]
+            lam = data["lam"]
+            phase = data["phase"]
+            trans = data["trans"]
+
+        # This data has a shape of [lenr=121, wavelength=441]
+        self.phase = np.angle(np.exp(1j * phase))
+        self.trans = np.sqrt(np.clip(data["trans"], 0, np.finfo(np.float32).max)).T
+        self.params = [rvec, lam]
+        # self.param_limits = [[rvec.min(), rvec.max()], [lam.min(), lam.max()]]
+        self.param_limits = [[15e-9, 135e-9], [310e-9, 750e-9]]
+
+        # Transform data into a cell-level dataset ([0, 1])
+        trans = self.trans
+        phase = self.phase
+        params = np.meshgrid(
+            *[
+                (p - l[0]) / (l[1] - l[0])
+                for (p, l) in zip(self.params, self.param_limits)
+            ],
+            indexing="ij",
+        )
+        self.x = np.stack([p.flatten() for p in params], -1)
+        self.y = np.stack(
+            [
+                trans.flatten(),
+                (np.cos(phase.flatten()) + 1) / 2,
+                (np.sin(phase.flatten()) + 1) / 2,
+            ],
+            -1,
+        )
+
+    def __len__(self):
+        return self.x.shape[0]
+
+    def __getitem__(self, idx):
+        return (self.x[idx], self.y[idx])
+
+    def plot(self, savepath=None):
+        phase = self.phase
+        trans = self.trans
+        r = self.params[0] * 1e9
+        lam = self.params[1] * 1e9
+
+        fig, ax = plt.subplots(1, 2, figsize=(12, 5))
+        ax[0].imshow(trans)
+        formatPlot(
+            fig,
+            ax[0],
+            xvec=r,
+            yvec=lam,
+            xlabel="radius (nm)",
+            ylabel="wavelength (nm)",
+            title="transmittance",
+            addcolorbar=True,
+            setAspect="auto",
+        )
+        ax[1].imshow(phase, cmap="hsv")
+        formatPlot(
+            fig,
+            ax[1],
+            xvec=r,
+            yvec=lam,
+            xlabel="radius (nm)",
+            ylabel="wavelength (nm)",
+            title="transmittance",
+            addcolorbar=True,
+            setAspect="auto",
+        )
+
+        if savepath is not None:
+            plt.savefig(savepath + self.__class__.__name__ + ".png")
+            plt.close()
+
+
+class Nanocylinders_TiO2_U350nm_H600nm(Dataset):
+    def __init__(self):
+        datpath = get_path_to_data(
+            "Nanocylinders_TiO2_Unit350nm_height600nm_FDTD.pickle"
+        )
+
+        with open(datpath, "rb") as file:
+            # Load the data from the file
+            data = pickle.load(file)
+            rvec = data["rvec"]
+            lam = data["lam"]
+            phase = data["phase"]
+            trans = data["trans"]
+
+        # This data has a shape of [lenr=121, wavelength=441]
+        self.phase = np.angle(np.exp(1j * phase))
+        self.trans = np.sqrt(np.clip(data["trans"], 0, np.finfo(np.float32).max)).T
+        self.params = [rvec, lam]
+        # self.param_limits = [[rvec.min(), rvec.max()], [lam.min(), lam.max()]]
+        self.param_limits = [[15e-9, 160e-9], [310e-9, 750e-9]]
+
+        # Transform data into a cell-level dataset ([0, 1])
+        trans = self.trans
+        phase = self.phase
+        params = np.meshgrid(
+            *[
+                (p - l[0]) / (l[1] - l[0])
+                for (p, l) in zip(self.params, self.param_limits)
+            ],
+            indexing="ij",
+        )
+        self.x = np.stack([p.flatten() for p in params], -1)
+        self.y = np.stack(
+            [
+                trans.flatten(),
+                (np.cos(phase.flatten()) + 1) / 2,
+                (np.sin(phase.flatten()) + 1) / 2,
+            ],
+            -1,
+        )
+
+    def __len__(self):
+        return self.x.shape[0]
+
+    def __getitem__(self, idx):
+        return (self.x[idx], self.y[idx])
+
+    def plot(self, savepath=None):
+        phase = self.phase
+        trans = self.trans
+        r = self.params[0] * 1e9
+        lam = self.params[1] * 1e9
+
+        fig, ax = plt.subplots(1, 2, figsize=(12, 5))
+        ax[0].imshow(trans)
+        formatPlot(
+            fig,
+            ax[0],
+            xvec=r,
+            yvec=lam,
+            xlabel="radius (nm)",
+            ylabel="wavelength (nm)",
+            title="transmittance",
+            addcolorbar=True,
+            setAspect="auto",
+        )
+        ax[1].imshow(phase, cmap="hsv")
+        formatPlot(
+            fig,
+            ax[1],
+            xvec=r,
+            yvec=lam,
+            xlabel="radius (nm)",
+            ylabel="wavelength (nm)",
+            title="transmittance",
+            addcolorbar=True,
+            setAspect="auto",
+        )
+
+        if savepath is not None:
+            plt.savefig(savepath + self.__class__.__name__ + ".png")
+            plt.close()
 
 
 class Nanofins_TiO2_U350nm_H600nm(Dataset):
@@ -124,87 +293,6 @@ class Nanofins_TiO2_U350nm_H600nm(Dataset):
             plt.close()
 
         return
-
-
-class Nanocylinders_TiO2_U180nm_H600nm(Dataset):
-    def __init__(self):
-        datpath = get_path_to_data("Nanocylinders_TiO2_Unit180nm_Height600nm_FDTD.mat")
-
-        # Phase and transmission has shape [wavelength=441, lenr=191]
-        # (Need to transpose to make wavelength last dimension)
-        # This is diameter not radius
-        data = scipy.io.loadmat(datpath)
-        self.phase = data["phase"].T
-        self.trans = np.sqrt(
-            np.clip(data["transmission"], 0, np.finfo(np.float32).max)
-        ).T
-        self.params = [
-            data["radius_m"],
-            data["wavelength_m"].flatten(),
-        ]
-        self.param_limits = [[30e-9, 150e-9], [310e-9, 750e-9]]
-
-        # Transform data into a cell-level dataset ([0, 1])
-        trans = self.trans
-        phase = self.phase
-        params = np.meshgrid(
-            *[
-                (p - l[0]) / (l[1] - l[0])
-                for (p, l) in zip(self.params, self.param_limits)
-            ],
-            indexing="ij",
-        )
-        self.x = np.stack([p.flatten() for p in params], -1)
-        self.y = np.stack(
-            [
-                trans.flatten(),
-                (np.cos(phase.flatten()) + 1) / 2,
-                (np.sin(phase.flatten()) + 1) / 2,
-            ],
-            -1,
-        )
-
-    def __len__(self):
-        return self.x.shape[0]
-
-    def __getitem__(self, idx):
-        return (self.x[idx], self.y[idx])
-
-    def plot(self, savepath=None):
-        phase = self.phase
-        trans = self.trans
-        r = self.params[0] * 1e9
-        lam = self.params[1] * 1e9
-
-        fig, ax = plt.subplots(1, 2, figsize=(12, 5))
-        ax[0].imshow(trans)
-        formatPlot(
-            fig,
-            ax[0],
-            xvec=r,
-            yvec=lam,
-            xlabel="radius (nm)",
-            ylabel="wavelength (nm)",
-            title="transmittance",
-            addcolorbar=True,
-            setAspect="auto",
-        )
-        ax[1].imshow(phase, cmap="hsv")
-        formatPlot(
-            fig,
-            ax[1],
-            xvec=r,
-            yvec=lam,
-            xlabel="radius (nm)",
-            ylabel="wavelength (nm)",
-            title="transmittance",
-            addcolorbar=True,
-            setAspect="auto",
-        )
-
-        if savepath is not None:
-            plt.savefig(savepath + self.__class__.__name__ + ".png")
-            plt.close()
 
 
 class Nanoellipse_TiO2_U350nm_H600nm(Nanofins_TiO2_U350nm_H600nm):
