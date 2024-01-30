13bfe897f8779ab900c97f5386bd866dd12bcac3
Dean Hazineh <dhazineh@g.harvard.edu>
2024-01-29 19:57:09 -0500

    explored option of alternative latents

--- dflat/metasurface/latent.py
+++ dflat/metasurface/latent.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def latent_to_param(z_latent, pmin=0, pmax=1):
+def latent_to_param(z_latent, pmin=0, pmax=1, func="tanh"):
     """Takes a latent tensor (defined on domain R[-inf, inf]) and returns the corresponding param tensor
         (defined on domain R[pmin, pmax]).
 
@@ -14,11 +14,18 @@ def latent_to_param(z_latent, pmin=0, pmax=1):
         `tf.float`: param tensor of equivalent shape
 
     """
+    assert func in ["sigmoid", "tanh", "sine"]
     z_latent = torch.tensor(z_latent) if not torch.is_tensor(z_latent) else z_latent
-    return ((torch.tanh(z_latent) + 1) / 2 * (pmax - pmin)) + pmin
+
+    if func == "tanh":
+        return ((torch.tanh(z_latent) + 1) / 2 * (pmax - pmin)) + pmin
+    elif func == "sigmoid":
+        return 1 / (1 + torch.exp(-z_latent))
+    elif func == "sine":
+        return torch.sin(z_latent)
 
 
-def param_to_latent(p_param, pmin=0.0, pmax=1.0):
+def param_to_latent(p_param, pmin=0.0, pmax=1.0, func="tanh"):
     """Takes a param tensor (defined on domain R[pmin, pmax]) and returns the corresponding latent tensor
     (defined on domain R[-inf, inf]).
 
@@ -30,5 +37,14 @@ def param_to_latent(p_param, pmin=0.0, pmax=1.0):
     Returns:
         `tf.float`: Latent tensor of equivalent shape
     """
+    assert func in ["sigmoid", "tanh", "sine"]
     p_param = torch.tensor(p_param) if not torch.is_tensor(p_param) else p_param
-    return torch.atanh((torch.clip(p_param, pmin, pmax) - pmin) / (pmax - pmin) * 2 - 1)
+
+    if func == "tanh":
+        return torch.atanh(
+            (torch.clip(p_param, pmin, pmax) - pmin) / (pmax - pmin) * 2 - 1
+        )
+    elif func == "sigmoid":
+        return torch.log(p_param / (1 - p_param))
+    elif func == "sine":
+        return torch.asin(p_param)
