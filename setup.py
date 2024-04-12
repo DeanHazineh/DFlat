51bbfca6c547accc78eb4652dab04e3dea073dc9
Dean Hazineh <dhazineh@g.harvard.edu>
2024-04-11 21:14:06 -0400

    Change name

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 
 setup(
     python_requires=">=3.9",
-    packages=find_packages(),
     include_package_data=True,
     package_data={
         "": ["*.txt", "*.csv"],
