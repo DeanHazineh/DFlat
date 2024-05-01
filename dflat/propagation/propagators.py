Hot fix to gradient checkpointing. Need to clean up finer details later.

dflat/propagation/propagators.py
@@ -226,9 +226,16 @@ class FresnelPropagation(BaseFrequencySpace):
             )
             self.out_phase.append(out_phase)
 
+        self.calc_in_dx = torch.tensor(self.calc_in_dx)
+        self.calc_out_dx = torch.tensor(self.calc_out_dx)
+        self.calc_samplesN = torch.tensor(self.calc_samplesN)
+        self.torch_zero = torch.tensor(0.0)
         return
 
     def forward(self, amplitude, phase, **kwargs):
+        return checkpoint(self._forward, amplitude, phase, **kwargs)
+
+    def _forward(self, amplitude, phase, **kwargs):
         """Propagates a complex field from an input plane to a planar output plane a distance out_distance_m.
 
         Args:
@@ -269,7 +276,7 @@ class FresnelPropagation(BaseFrequencySpace):
         amplitude, phase = self._regularize_field(amplitude, phase)
 
         # propagate by the fresnel method
-        amplitude, phase = checkpoint(self.fresnel_transform, amplitude, phase)
+        amplitude, phase = self.fresnel_transform(amplitude, phase)
 
         # Transform field back to the specified output grid
         amplitude, phase = self._resample_field(amplitude, phase)
@@ -292,7 +299,7 @@ class FresnelPropagation(BaseFrequencySpace):
         radial_symmetry = self.radial_symmetry
         dtype = amplitude[0].dtype
         device = amplitude[0].device
-        torch_zero = torch.tensor(0.0, dtype=dtype, device=device)
+        torch_zero = self.torch_zero.to(dtype=dtype, device=device)
 
         for i, lam in enumerate(self.wavelength_set):
             in_quad_term = self.quad_term_in[i].to(dtype=dtype, device=device)
@@ -300,37 +307,25 @@ class FresnelPropagation(BaseFrequencySpace):
                 torch.complex(torch_zero, phase[i] + in_quad_term)
             )
 
-            # propagate with qdht or fft2
             if radial_symmetry:
                 fx = self.ang_fx[i].to(dtype=dtype, device=device)
                 x = torch.squeeze(self.x[i].to(dtype=dtype, device=device))
-                norm = torch.tensor(
-                    1
-                    / np.sqrt(
-                        (
-                            np.prod(self.calc_in_dx[i])
-                            * np.prod(self.calc_out_dx[i])
-                            * np.prod(self.calc_samplesN[i])
-                        )
-                    ),
-                    dtype=dtype,
-                    device=device,
-                )
+                norm = 1.0 / torch.sqrt(
+                    torch.prod(self.calc_in_dx[i])
+                    * torch.prod(self.calc_out_dx[i])
+                    * torch.prod(self.calc_samplesN[i])
+                ).to(dtype=dtype, device=device)
                 norm = torch.complex(norm, torch_zero)
                 kr, wavefront = qdht(x, transform_term)
                 wavefront = (
                     general_interp_regular_1d_grid(kr / 2 / np.pi, fx, wavefront) * norm
                 )
             else:
-                norm = torch.tensor(
-                    np.sqrt(
-                        np.prod(self.calc_in_dx[i])
-                        / np.prod(self.calc_out_dx[i])
-                        / np.prod(self.calc_samplesN[i])
-                    ),
-                    dtype=torch.float32,
-                    device=device,
-                )
+                norm = torch.sqrt(
+                    torch.prod(self.calc_in_dx[i])
+                    / torch.prod(self.calc_out_dx[i])
+                    / torch.prod(self.calc_samplesN[i])
+                ).to(dtype=dtype, device=device)
                 norm = torch.complex(norm, torch_zero)
                 wavefront = fftshift(fft2(ifftshift(transform_term))) * norm
 
@@ -478,6 +473,9 @@ class ASMPropagation(BaseFrequencySpace):
             print(f"   - Resampling to grid size: {self.out_resample_dx}")
 
     def forward(self, amplitude, phase, **kwargs):
+        return checkpoint(self._forward, amplitude, phase, **kwargs)
+
+    def _forward(self, amplitude, phase, **kwargs):
         """Propagates a complex field from an input plane to a planar output plane a distance out_distance_m.
 
         Args:
@@ -518,7 +516,7 @@ class ASMPropagation(BaseFrequencySpace):
         amplitude, phase = self._regularize_field(amplitude, phase)
 
         # propagate by the asm method
-        amplitude, phase = checkpoint(self.ASM_transform, amplitude, phase)
+        amplitude, phase = self.ASM_transform(amplitude, phase)
 
         # Transform field back to the specified output grid and convert to 2D
         amplitude, phase = self._resample_field(amplitude, phase)

dflat/render/fft_convolve.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.nn.functional as F
+from torch.utils.checkpoint import checkpoint
 from torch.fft import fftshift, ifftshift, fft2, ifft2, rfft2, irfft2
 from dflat.radial_tranforms import resize_with_crop_or_pad
 
@@ -47,7 +48,8 @@ def general_convolve(image, filter, rfft=False):
     filter_resh = resize_with_crop_or_pad(filter, *image_shape[-2:], radial_flag=False)
 
     ### Run the convolution
-    image = torch.real(fourier_convolve(image, filter_resh, rfft))
+    image = checkpoint(fourier_convolve, image, filter_resh, rfft)
+    image = torch.real(image)
 
     ### Undo odd padding if it was done before FFT
     image = resize_with_crop_or_pad(image, *init_image_shape[-2:], False)

dflat/render/fronto_planar_renderer.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from einops import rearrange
+from torch.utils.checkpoint import checkpoint
 
 from .fft_convolve import general_convolve
 from dflat.radial_tranforms import resize_with_crop_or_pad
@@ -12,7 +13,16 @@ class Fronto_Planar_Renderer_Incoherent(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(
+    def forward(self, psf_intensity, scene_radiance, rfft=True, crop_to_psf_dim=False):
+        return checkpoint(
+            self._forward,
+            psf_intensity,
+            scene_radiance,
+            rfft,
+            crop_to_psf_dim,
+        )
+
+    def _forward(
         self,
         psf_intensity,
         scene_radiance,
@@ -27,6 +37,7 @@ class Fronto_Planar_Renderer_Incoherent(nn.Module):
         # scene radiance [1 P Z L H W]
 
         assert len(psf_shape) == 6, "PSF should be rank 6 tensor like [B P Z L H W]."
+
         assert (
             len(srad_shape) == 6
         ), "Scene radiance should be passed as a rank 5 tensor like [P Z L H W]"