f5726fa97aa08984e5a083f6a4c508c3e4018fe1
Dean Hazineh <dhazineh@g.harvard.edu>
2024-08-31 12:52:07 -0400

    readthedocs

--- source/conf.py
+++ source/conf.py
@@ -23,8 +23,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
 extensions = [
     "sphinx_design",
 ]
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
